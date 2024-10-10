84ffa53647f1fc2c3d8ae3478aef4cab753e4d72
AvivGet <avivgetraidman@gmail.com>
2024-10-10 16:18:00 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,7 @@
+// Add fade-in class to the main content when the page loads
+window.addEventListener('DOMContentLoaded', () => {
+  const mainContent = document.getElementById('main-content');
+  mainContent.classList.add('fade-in');
+});
+
+
